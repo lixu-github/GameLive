c23588695dbae6c7497d8c5d5a48f86abea0bf8d
lixu <lixu_sx@qiyi.com>
2017-08-12 20:10:17 +0800

    add

---
+++ GameLive/GameLiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameLiveViewController.h
+//  GameLive
+//
+//  Created by iqiyi on 17/8/11.
+//  Copyright © 2017年 iqiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameLiveViewController : UIViewController
+
+@end
